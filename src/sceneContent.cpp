150333b057a4954009a24190df8cc55ae410b746
Taeke Roukema <taeke@taekeroukema.xyz>
2022-12-16 20:51:08 +0100

    Added scenes folder

--- src/sceneContent.cpp
+++ src/sceneContent.cpp
@@ -2,22 +2,41 @@
 
 void initScene(Scene *scene) {
 
-    scene -> AddObject(new SDFIntersection(
-        {0, 0, 10}, // center
-        new SDFSphere(
-            (Vector3){1, 0, 0}, // center
-            2, // radius
+    scene -> AddObject(new SDFSubtraction(
+        {0, 0, 10},
+        new SDFIntersection(
+            {0, 0, 0},
+            new SDFSphere(
+                {0,0,0},
+                1,
+                ObjectMaterial()
+            ),
+            new SDFSphere(
+                {0.5,0,-.3},
+                1,
+                ObjectMaterial()
+            ),
             ObjectMaterial()
         ),
         new SDFSphere(
-            (Vector3){-1, 0, 0.5}, // center
-            1, // radius
+            {0.5, 0.3, -0.1},
+            0.7,
             ObjectMaterial()
-        ), 
-        ObjectMaterial (
-            (Color){255, 0, 0, 255}, // color
-            500, // specular
-            0.2 // reflective
+        ),
+        ObjectMaterial(
+            {255, 0, 0, 255},
+            100,
+            0.2
+        )
+    ));
+
+    scene -> AddObject(new SDFTorus(
+        {0, 0, 10},
+        {2,0.2},
+        ObjectMaterial(
+            {255, 255, 0, 255},
+            100,
+            0.2
         )
     ));
 
@@ -29,12 +48,12 @@ void initScene(Scene *scene) {
     scene -> AddLight(Light(
         1, // Point
         0.6,
-        (Vector3){2, 1, 0}
+        {2, 1, 0}
     ));
 
     scene -> AddLight(Light(
         2, // Directional
         0.2,
-        (Vector3){1, 4, 4}
+        {0, 4, 4}
     ));
 }
